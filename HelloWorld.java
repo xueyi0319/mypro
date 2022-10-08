21de0c0c0c5f998b321848b8940f5b520cb94690
luoxueyi <10140418+luoxueyi@user.noreply.gitee.com>
2022-10-08 09:43:32 +0800

    v1.0

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+/*
+The traditional HelloWorld example
+ */
+public class HelloWorld {
+
+    public static void main(String args[]){
+        int x=6,y=8;
+        boolean b;
+        System.out.println(b=x>y&&++x==--y);
+        System.out.println(x);
+        System.out.println(y);
+    }
+}
